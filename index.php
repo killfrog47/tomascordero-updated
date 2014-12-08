abb1f79ecc06a5028de9d97cb77123b5711395ff
Tomas Cordero <tcorderoiv@gmail.com>
2014-12-07 19:00:20 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="assets/css/master.css">
+</head>
+<body>
+	<footer>
+		<div class="bread-crums"></div>
+		<div class="navigation"></div>
+	</footer>
+</body>
+</html>
